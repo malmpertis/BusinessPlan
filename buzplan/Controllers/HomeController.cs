7523b7dab7acbbe6e561fd11aaa40b728d8f0031
Michael Almpertis <mike.almpertis@gmail.com>
2017-09-29 13:38:18 +0300

    Add project files.

---
+++ buzplan/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using buzplan.Models;
+
+namespace buzplan.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult GetKad(string keyword)
+        {
+            using(var db=new businessPlanEntities())
+            {
+                var result = db.bp_kad.Where(c => c.id.StartsWith(keyword)||c.title.Contains(keyword)).OrderBy(c => c.id).Take(10).ToList();
+                return Json(result);
+            }
+        }
+        
+    }
+}
---
+++ buzplan/html/js/GeoFactory.js
@@ -0,0 +1,8 @@
+﻿angular.module('myApp')
+    .factory('geoFactory', ['$http', '$rootScope', function ($http, $rootScope) {
+        return {
+            getByParentId: function (pid) {
+                return $http.post('Geo/GetByPid', { pid: pid });
+            }
+        }
+    }]);
