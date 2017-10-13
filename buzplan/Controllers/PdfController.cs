35850128bd4674532094b801c7f30f111c80e36a
kkoufogiannis <kkoufogiannis@apopsi.gr>
2017-10-13 16:37:16 +0300

    13102017

---
+++ buzplan/Controllers/PdfController.cs
@@ -0,0 +1,31 @@
+﻿using buzplan.Models;
+using buzplan.ObjectClasses;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace buzplan.Controllers
+{
+    public class PdfController : Controller
+    {
+
+        public ActionResult Header(string titlos)
+        {
+            ViewBag.titlos = titlos;
+            return View();
+
+        }
+        public ActionResult Footer()
+        {
+            string UserId = User.Identity.GetUserId();
+            using (var db = new businessPlanEntities())
+            {
+                var returned = Plan.GetPlan(UserId, db);
+                return View(returned);
+            }
+        }
+    }
+}
--- buzplan/Controllers/PlanController.cs
+++ buzplan/Controllers/PlanController.cs
@@ -1,5 +1,7 @@
 ﻿using buzplan.Models;
+using buzplan.ObjectClasses;
 using Microsoft.AspNet.Identity;
+using Rotativa.MVC;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +10,9 @@ using System.Web.Mvc;
 
 namespace buzplan.Controllers
 {
+    [Authorize]
     public class PlanController : Controller
     {
-        // GET: Plan
-
         public ActionResult Get(string UserId)
         {
             UserId = User.Identity.GetUserId();
@@ -22,6 +23,7 @@ namespace buzplan.Controllers
                              select c).ToDictionary(c => c.Item, c => c.Data));
             }
         }
+
         public ActionResult setImg(HttpPostedFileBase file)
         {
             string UserId = User.Identity.GetUserId();
@@ -32,8 +34,10 @@ namespace buzplan.Controllers
                 url = "/" + userImage
             });
         }
+
         public ActionResult Set(Dictionary<string, string> plan, string UserId)
         {
+            string[] reject = { "action", "page", "controller" };
             UserId = User.Identity.GetUserId();
             using (var db = new businessPlanEntities())
             {
@@ -47,7 +51,7 @@ namespace buzplan.Controllers
                             db.PlanItems.RemoveRange(q);
                             db.SaveChanges();
                         }
-                        db.PlanItems.AddRange(plan.Select(c => new PlanItems
+                        db.PlanItems.AddRange(plan.Where(c=> !reject.Contains(c.Key)).Select(c => new PlanItems
                         {
                             UserId = UserId,
                             Data = c.Value,
@@ -65,6 +69,123 @@ namespace buzplan.Controllers
                     }
                 }
 
+            }
+
+        }
+
+        [AllowAnonymous]
+        public ActionResult createDiag1(string UserId)
+        {
+            using (var db = new businessPlanEntities())
+            {
+                var returned = Plan.GetPlan(UserId, db);
+                return View(returned);
+            }
+        }
+
+        [AllowAnonymous]
+        public ActionResult createDiag2(string UserId)
+        {
+            using (var db = new businessPlanEntities())
+            {
+                var returned = Plan.GetPlan(UserId, db);
+                return View(returned);
+            }
+        }
+
+        [AllowAnonymous]
+        public ActionResult createDiag3(string UserId)
+        {
+            using (var db = new businessPlanEntities())
+            {
+                var returned = Plan.GetPlan(UserId, db);
+                return View(returned);
+            }
+        }
+
+        [AllowAnonymous]
+        public ActionResult createDiag4(string UserId)
+        {
+            using (var db = new businessPlanEntities())
+            {
+                var returned = Plan.GetPlan(UserId, db);
+                return View(returned);
+            }
+        }
+
+        [AllowAnonymous]
+        public ActionResult createDiag5(string UserId)
+        {
+            using (var db = new businessPlanEntities())
+            {
+
+                var returned = Plan.GetPlan(UserId, db);
+                return View(returned);
+            }
+        }
+
+        [AllowAnonymous]
+        public ActionResult createDiag6(string UserId)
+        {
+            using (var db = new businessPlanEntities())
+            {
+
+                var returned = Plan.GetPlan(UserId, db);
+                return View(returned);
+            }
+        }
+
+        public ActionResult Pdftest()
+        {
+            string UserId = User.Identity.GetUserId();
+            using (var db = new businessPlanEntities())
+            {
+                var returned = Plan.GetPlan(UserId, db);
+                return View("Pdf", returned);
+            }
+
+        }
+
+        public ActionResult Pdf()
+        {
+            
+
+
+
+            string UserId = User.Identity.GetUserId();
+            ViewBag.UserId = UserId;
+            using (var db = new businessPlanEntities())
+            {
+                var returned = Plan.GetPlan(UserId, db);
+                
+                if (returned == null)
+                {
+                    Response.StatusCode = 500;
+                    return null;
+                }
+                string customSwitches = string.Format(
+                 //"--print-media-type " +
+                 //"--allow \"{0}\" \"{1}\" " +
+                 "--header-html \"{0}\" " +
+                 "--footer-html \"{1}\" " +
+                 "--load-error-handling ignore " +
+                 "--header-spacing \"10\" " +
+                 "--footer-spacing \"10\" " +
+                 "--footer-font-size \"10\" " +
+                 "--header-font-size \"10\" " +
+                 //"--footer-left \"Εμπιστευτικό – Να μην διανεμηθεί\" " +
+                 //"--footer-center \"[page]/[toPage]\"" +
+                 "",
+                 //" --load-error-handling ignore",
+                 Url.Action("Header", "Pdf", new { titlos = returned.company.title }, "http"),
+                 Url.Action("Footer", "Pdf", new { }, "http")
+             );
+                Response.Headers.Add("Content-Disposition:", "attachment; filename=\"BusinessPlan.pdf\"");
+                return new ViewAsPdf(returned)
+                {
+                    RotativaOptions = new Rotativa.Core.DriverOptions { CustomSwitches = customSwitches }
+                    
+                }; 
             }
 
         }
